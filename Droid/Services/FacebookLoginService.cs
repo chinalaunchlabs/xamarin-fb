79e50bd114ddd16dd4cd8ba8ec51e0d6b6cc0924
China Yu <china_yu@launchlabs.ph>
2016-03-07 14:20:05 +0800

    Unsubscribed from facebook login events

--- Droid/Services/FacebookLoginService.cs
+++ Droid/Services/FacebookLoginService.cs
@@ -8,6 +8,7 @@ using Android.Content;
 using Xamarin.Facebook;
 using System.Collections.Generic;
 using Android.OS;
+using System.Threading;
 
 [assembly:Xamarin.Forms.Dependency(typeof(FacebookLoginService))]
 namespace Wiggin.Facebook.Droid
@@ -15,31 +16,42 @@ namespace Wiggin.Facebook.Droid
 	public class FacebookLoginService: global::Xamarin.Forms.Platform.Android.FormsApplicationActivity, IFacebookLogin
 	{
 		private DroidAccessToken _accessToken = null;
+		private TaskCompletionSource<IAccessToken> tcs;
 
 		public Task<IAccessToken> LogIn(string[] permissions) {
-			TaskCompletionSource<IAccessToken> tcs = new TaskCompletionSource<IAccessToken> ();
+			tcs = new TaskCompletionSource<IAccessToken> ();
 
-			FacebookLoginActivity.OnFacebookLoginSuccess += () => {
-				_accessToken = new DroidAccessToken(AccessToken.CurrentAccessToken);
-				tcs.SetResult(_accessToken);
-			};
-
-			FacebookLoginActivity.OnFacebookLoginCancel += () => {
-				tcs.SetCanceled();
-			};
-
-			FacebookLoginActivity.OnFacebookLoginError += () => {
-				_accessToken = new DroidAccessToken(AccessToken.CurrentAccessToken);
-				tcs.SetResult(_accessToken);
-			};
-
+			FacebookLoginActivity.OnFacebookLoginCancel += FacebookLoginActivity_OnFacebookLoginCancel;
+			FacebookLoginActivity.OnFacebookLoginSuccess += FacebookLoginActivity_OnFacebookLoginSuccess;
+			FacebookLoginActivity.OnFacebookLoginError += FacebookLoginActivity_OnFacebookLoginError;
 
 			var activity = Forms.Context as Activity;
 			var myIntent = new Intent (activity, typeof(FacebookLoginActivity));
 			myIntent.PutExtra("permissions", permissions);
+	
 			activity.StartActivityForResult (myIntent, 0);
 
 			return tcs.Task;
+		}
+
+		void FacebookLoginActivity_OnFacebookLoginError ()
+		{
+			_accessToken = new DroidAccessToken(AccessToken.CurrentAccessToken);
+			tcs.SetResult(_accessToken);
+			FacebookLoginActivity.OnFacebookLoginError -= FacebookLoginActivity_OnFacebookLoginError;
+		}
+
+		void FacebookLoginActivity_OnFacebookLoginSuccess ()
+		{
+			_accessToken = new DroidAccessToken(AccessToken.CurrentAccessToken);
+			tcs.SetResult(_accessToken);
+			FacebookLoginActivity.OnFacebookLoginSuccess -= FacebookLoginActivity_OnFacebookLoginSuccess;
+		}
+
+		void FacebookLoginActivity_OnFacebookLoginCancel ()
+		{
+			tcs.SetCanceled();
+			FacebookLoginActivity.OnFacebookLoginCancel -= FacebookLoginActivity_OnFacebookLoginCancel;
 		}
 
 		public bool IsLoggedIn() {
@@ -50,6 +62,12 @@ namespace Wiggin.Facebook.Droid
 			if (!IsLoggedIn ())
 				return null;
 			return _accessToken;
+		}
+
+		private void LoginSuccessCallback () {
+			_accessToken = new DroidAccessToken(AccessToken.CurrentAccessToken);
+			System.Diagnostics.Debug.WriteLine("Facebook login success");
+			tcs.SetResult(_accessToken);
 		}
 	}
 }
